20b8c502016bf34fb66563ffc30d3db682d7c2af
zhaojr <1053668864@qq.com>
2016-08-25 14:43:19 +0800

    add svg

--- app/src/main/java/com/opklm78/momoda/FragmentMine.java
+++ app/src/main/java/com/opklm78/momoda/FragmentMine.java
@@ -7,9 +7,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-public class FragmentMine extends Fragment {
-
+public class FragmentMine extends Fragment implements View.OnClickListener{
+    Button ib = null;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
@@ -26,5 +30,16 @@ public class FragmentMine extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        ib = (Button) getActivity().findViewById(R.id.fm_ib_about);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(getActivity().findViewById(v.getId())==ib){
+            WebView wv = new WebView(this.getContext());
+            wv.loadUrl("file:///assets/licenses.html");
+            wv.setWebViewClient(new WebViewClient());
+        }
     }
 }
